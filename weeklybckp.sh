25c9b84c8bb67c2812b5e36272e84a53144faa6b
Pendrag00n <88731722+Pendrag00n@users.noreply.github.com>
2023-03-29 15:34:22 +0100

    Update weeklybckp.sh
    Switched to tar to keep permisions and ownership, added support for network locations appart for information on the tty as well as the backup log

--- weeklybckp.sh
+++ weeklybckp.sh
@@ -2,7 +2,7 @@
 
 #
 # PDGBCKP
-# LIGHTWEIGHT (AND PRIMITIVE) BACKUP TOOL
+# LIGHTWEIGHT BACKUP TOOL
 #
 
 dirname=$(date +%d-%m-%Y_%H-%M-%S)
@@ -10,38 +10,69 @@ logdate=$(date +%d-%m-%Y)
 
 #////////////////////////////////////
 # VARS (Modify accordingly)
-	backuppath="/var/backups/pendragon"
-	logpath="/home/pendragon"
+	backuppath="//192.168.1.2/share" # Path where the backup will be stored
+		remotebackuppath="true" # Set to true if you are backing up to a remote path
+		mountpath="/tmp/packup" # Only used if remotebackuppath is set to true
+	logpath="/home/pendragon" # Path to the log file	
 	files="/etc/ /home/pendragon/" # Enter files/folders separated by a space
-	compressionlvl="6" # Where 0 is no compression and 9 is maximum compression (6 is default)
+	compressionlvl="4" # Where 1 is no compression and 9 is maximum compression (6 is default)
+	
 #////////////////////////////////////
 
 # Start script
-if ! which zip >/dev/null; then
-	apt install zip -y
-	echo "[ $logdate ]: zip was automatically installed" >> $logpath/backups.log
+if ! which tar >/dev/null; then
+	apt install tar -y
+	echo "[ $logdate ]: tar was surprisingly not installed and was automatically installed" >> $logpath/backups.log
 fi
+
 if [ ! -d $backuppath ]; then
 	mkdir -p $backuppath
     exit
 fi
 
-zip -r -$compressionlvl $backuppath/pdgbackup_$dirname.zip $files &> $logpath/temp_backups_error.log
+# Mount backup path
+if [ $remotebackuppath = "true" ]; then
+	mount -t cifs //192.168.1.2/share $mountpath -o credentials=.ales.txt
+	sleep 3 #Should work without this line
+fi
+
+if [ $? -ne 0 ]; then
+	echo "The remote path could not be mounted :( Exiting script"
+	echo "[ $logdate ]: The remote path could not be mounted :( Exiting script" >> $logpath/backups.log
+	exit
+fi
+
+# zip -r -$compressionlvl $backuppath/packup_$dirname.zip $files &> $logpath/temp_backups_error.log
+tar -czf -$compressionlvl $backuppath/packup_$dirname.tar.gz $files &> $logpath/temp_backups_error.log
+
 
 if [ $? -eq 0 ]; then
-    echo "[ $logdate ]: pdgbackup_$dirname.zip was created in $backuppath" >> $logpath/backups.log
-    chmod 600 pdgbackup_$dirname.zip
+	echo "packup_$dirname.zip was created in $backuppath (Took $SECONDS seconds and weighs $(du -sh $backuppath/packup_$dirname.zip | awk '{print $1}') )"
+	echo "[ $logdate ]: packup_$dirname.zip was created in $backuppath (Took $SECONDS seconds and weighs $(du -sh $backuppath/packup_$dirname.zip | awk '{print $1}'))" >> $logpath/backups.log
+	chmod 600 $backuppath/packup_$dirname.zip
 else
-    echo "[ $logdate ]: Backup exited with errors and the zipfile was deleted :(" >> $logpath/backups.log
-    if [ -f $backuppath/pdgbackup_$dirname.zip ]; then
-        rm -rf $backuppath/pdgbackup_$dirname.zip
-    fi
-    if [ -s $logpath/temp_backups_error.log ]; then
-        echo "Encountered the following errors:" >> $logpath/backups.log
-        grep "zip warning:" $logpath/temp_backups_error.log >> $logpath/backups.log
-	echo ""
-    fi
-    if [ -f $logpath/temp_backups_error.log ]; then
-        rm -f $logpath/temp_backups_error.log
-    fi
+	echo "Backup exited with errors and the zipfile was deleted :("
+	echo "[ $logdate ]: Backup exited with errors and the zipfile was deleted :(" >> $logpath/backups.log
+
+	if [ -f $backuppath/packup_$dirname.zip ]; then
+        	rm -rf $backuppath/packup_$dirname.zip
+	fi
+
+	if [ -s $logpath/temp_backups_error.log ]; then
+		echo "Encountered the following errors:" >> $logpath/backups.log
+		grep "zip warning:" $logpath/temp_backups_error.log >> $logpath/backups.log
+		echo ""
+	fi
+
+	if [ -f $logpath/temp_backups_error.log ]; then
+		rm -f $logpath/temp_backups_error.log
+	fi
+	
 fi
+
+if [ $remotebackuppath = "true" ]; then
+		umount /home/julio/mount
+fi
+# Check for backups older than 3 months inside $backuppath and delete them
+# find $backuppath -type f -mtime +90 -exec rm -f {} \;
+exit
