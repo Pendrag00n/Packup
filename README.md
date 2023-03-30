1ae68a7b9c73e0b9516b2c4a6bd2f0045fec6153
Pendrag00n <juliofelipeHI@gmail.com>
2023-03-30 11:30:40 +0100

    Cleanup and minor fixes

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Packup
-LIGHTWEIGHT (AND PRIMITIVE) BACKUP TOOL INTENDED TO BE RUN BY CRON
+LIGHTWEIGHT BACKUP TOOL INTENDED TO BE RUN BY CRON (CAN ALSO BE RUN MANUALLY)
 
 ## USAGE
-* Store the script inside a folder used for cron scripts (or dont), for example, */usr/scripts/*.
+* Store the script (alongside *.ales.txt* if needed) inside a folder used for cron scripts (or dont), for example, */usr/scripts/*.
 * Modify the variables inside the script according to your needs.
-* Open root's crontab by running `sudo crontab -e` (Running this as another user may cause the backup to fail due to permissions, but it's not mandatory)
-* Configure the scheduled job to run the script. For example: `0 3 * * 1 /bin/bash /usr/scripts/weeklybckp.sh` executes the backup every monday at 3 a.m
+* Open root's crontab by running `sudo crontab -e` (Running this as another user WILL cause the backup to fail due to permissions issues).
+* Configure the scheduled job to run the script. For example: 
+`0 3 * * 1 /bin/bash /usr/scripts/weeklybckp.sh` 
+executes the backup every monday at 3 a.m
--- packup.sh
+++ packup.sh
@@ -5,23 +5,26 @@
 # LIGHTWEIGHT BACKUP TOOL
 #
 
-dirname=$(date +%d-%m-%Y_%H-%M-%S)
-logdate=$(date +%d-%m-%Y)
+#//////////////////////////////
+#		CONFIGURATION
+#    (Modify accordingly)
 
-#////////////////////////////////////
-# VARS (Modify accordingly)
 	backuppath="//192.168.1.2/share" # Path where the backup will be stored
 		remotebackuppath="true" # Set to true if you are backing up to a remote path
 		mountpath="/tmp/packup" # Only used if remotebackuppath is set to true
-	logpath="/home/pendragon" # Path to the log file	
-	files="/etc/ /home/pendragon/" # Enter files/folders separated by a space
+	logpath="/home/julio" # Path to the log file
+	files="/etc/" # Enter files/folders separated by a space
 	compressionlvl="4" # Where 1 is no compression and 9 is maximum compression (6 is default)
-	
-#////////////////////////////////////
+
+#///////////////////////////////
+
+dirname=$(date +%d-%m-%Y_%H-%M-%S)
+logdate=$(date +%d-%m-%Y)
 
 # Start script
 if ! which tar >/dev/null; then
 	apt install tar -y
+	echo "tar was surprisingly not installed and is being installed now"
 	echo "[ $logdate ]: tar was surprisingly not installed and was automatically installed" >> $logpath/backups.log
 fi
 
@@ -30,10 +33,23 @@ if [ ! -d $backuppath ]; then
     exit
 fi
 
-# Mount backup path
+# Test credentials file (ales.txt) and mount backup path
 if [ $remotebackuppath = "true" ]; then
-	mount -t cifs //192.168.1.2/share $mountpath -o credentials=.ales.txt
-	sleep 3 #Should work without this line
+	if [ -f ales.txt ]; then
+		chown root:root ales.txt
+		chmod 600 ales.txt
+		if [ ! -d $mountpath ]; then
+			mkdir -p $mountpath
+			chown root:root $mountpath
+			chmod 600 $mountpath
+		fi
+		mount -t cifs $backuppath $mountpath -o credentials=ales.txt
+		sleep 4 #Should work without this line
+	else
+		echo "The credentials file (ales.txt) was not found :( Exiting script"
+		echo "[ $logdate ]: The credentials file (ales.txt) was not found :( Exiting script" >> $logpath/backups.log
+		exit 2
+	fi
 fi
 
 if [ $? -ne 0 ]; then
@@ -61,17 +77,17 @@ else
 	if [ -s $logpath/temp_backups_error.log ]; then
 		echo "Encountered the following errors:" >> $logpath/backups.log
 		grep "zip warning:" $logpath/temp_backups_error.log >> $logpath/backups.log
-		echo ""
+		echo "" >> $logpath/backups.log
 	fi
 
 	if [ -f $logpath/temp_backups_error.log ]; then
 		rm -f $logpath/temp_backups_error.log
 	fi
-	
+
 fi
 
 if [ $remotebackuppath = "true" ]; then
-		umount /home/julio/mount
+		umount $mountpath
 fi
 # Check for backups older than 3 months inside $backuppath and delete them
 # find $backuppath -type f -mtime +90 -exec rm -f {} \;
