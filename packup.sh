3a1ea2909d5d26d1883b645fc4247dec95ce18c3
Pendrag00n <88731722+Pendrag00n@users.noreply.github.com>
2023-04-21 15:00:47 +0100

    Added comments and more strict permissions
    Script seems finished an ready to go :)

--- packup.sh
+++ packup.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 #
-# PACKUP
+#   PACKUP
 # LIGHTWEIGHT BACKUP TOOL
 # https://github.com/Pendrag00n/Packup
 # ShellCheck Standards Compliant
 #
 
 #//////////////////////////////
-#	CONFIGURATION
+#       CONFIGURATION
 #    (Modify accordingly)
 
 backuppath="/var/packup" # Path where the backup will be stored. You can use a remote path by using the format //ip/share
 logpath="/home/$SUDO_USER" # Path to the log file
 logfile="packup.log" # Name of the log file
 files="/etc/ /home/$SUDO_USER/Documents/" # Enter files/folders separated by a space
+backuppermission="0600" # Permission of the backup file
+
     # Remote backup variables:
     mountpath="/home/$SUDO_USER/packuptmp" # This variable is only used if backup up to a remote location
     mountport="445" # Default SMB/CIFS port
@@ -44,6 +46,14 @@ if [ "${logpath: -1}" = "/" ]; then
     logpath=${logpath::-1}
 fi
 
+# Test if the log path and log file exists
+if [ ! -d "$logpath" ]; then
+    mkdir -p "$logpath"
+fi
+if [ ! -f "$logpath"/$logfile ]; then
+    touch "$logpath"/$logfile
+fi
+
 # Check if the files exist
 for file in $files; do
     if ! test -e "$file"; then
@@ -54,7 +64,7 @@ for file in $files; do
 done
 
 
-# Start script
+# Check if tar is installed
 if ! which tar >/dev/null; then
     apt install tar -y
     echo "tar was surprisingly not installed and is being installed now"
@@ -65,7 +75,7 @@ fi
 if [ "$remotebackuppath" = "true" ]; then
     if [ -f ales.txt ]; then
         chown root:root ales.txt
-        chmod 600 ales.txt
+        chmod 0600 ales.txt
         #Test if the IP provided has port 445 enabled
             IP=$(echo "$backuppath" | cut -d/ -f3)
             echo "Testing if share is up on port 445..."
@@ -76,25 +86,40 @@ if [ "$remotebackuppath" = "true" ]; then
                 exit 1
             fi
         
+        # Check if the mountpath exists, if not, create it.
         if [ ! -d "$mountpath" ]; then
             mkdir -p "$mountpath"
         fi
             chown root:root "$mountpath"
-            chmod 600 "$mountpath"
+            chmod 0600 "$mountpath"
         # Check if the backup path is already mounted
         if mount | grep -q "$backuppath .*$mountpath"; then
             echo "Drive seems to be mounted to the mountpath already!"
         else
             # Mount the network drive location into the mountpath
             echo "Mounting remote path..."
-            mount -t cifs "$backuppath" "$mountpath" -o credentials="$PWD"/ales.txt
-            if ! mount -t cifs "$backuppath" "$mountpath" -o credentials="$PWD"/ales.txt;
+            mount -t cifs "$backuppath" "$mountpath" -o credentials="$PWD"/ales.txt &> "$logpath"/temp_mount_error.log
+            # if temp_mount_error.log exists, change it's permissions to 0600
+            if [ -f "$logpath"/temp_mount_error.log ]; then
+                chown root:root "$logpath"/temp_mount_error.log
+                chmod 0600 "$logpath"/temp_mount_error.log
+            fi
             sleep 1 #Should work without this line
-            then
+            if ! mount -t cifs "$backuppath" "$mountpath" -o credentials="$PWD"/ales.txt; then
                 echo "The remote path could not be mounted :( Exiting script"
-                echo "[ $logdate ]: The remote path could not be mounted :( Exiting script" >> "$logpath"/$logfile
+                {
+                echo "[ $logdate ]: The remote path could not be mounted :( Exiting script"
+                echo -n "Reasons for failure:"
+                cat "$logpath"/temp_mount_error.log
+                } >> "$logpath"/$logfile
                 exit 2
             fi
+
+            # If temp_mount_error.log is not empty, delete it
+            if [ -s "$logpath"/temp_mount_error.log ]; then
+                rm "$logpath"/temp_mount_error.log
+            fi
+
             sleep 1 #Should work without this line
         fi
     else
@@ -109,26 +134,39 @@ if [ ! -d "$backuppath" ]; then
     mkdir -p "$backuppath"
 fi
 
+# Do the backup and determine if it failed or not
+failed="false"
 if [ "$remotebackuppath" = "true" ]; then
-    tar -cf "$mountpath"/packup_"$dirname".tar.gz "$files" &> "$logpath"/temp_backups_error.log
+    tar -cpf "$mountpath"/packup_"$dirname".tar.gz "$files" &> "$logpath"/temp_backups_error.log
     backuppath=$mountpath
+    if ! tar -cpf "$mountpath"/packup_"$dirname".tar.gz "$files"; then
+        failed="true"
+    fi
 else
-    tar -cf "$backuppath"/packup_"$dirname".tar.gz "$files" &> "$logpath"/temp_backups_error.log
+    tar -cpf "$backuppath"/packup_"$dirname".tar.gz "$files" &> "$logpath"/temp_backups_error.log
+    if ! tar -cpf "$backuppath"/packup_"$dirname".tar.gz "$files"; then
+        failed="true"
+    fi
 fi
 
-if [ $? -eq 0 ]; then
+# 
+if [ "$failed" = "false" ]; then
+    chmod $backuppermission "$backuppath"/packup_"$dirname".tar.gz
     echo "packup_$dirname.tar.gz was created in $backuppath (Took $SECONDS seconds and weighs $(du -sh "$backuppath"/packup_"$dirname".tar.gz | awk '{print $1}') )"
     echo "[ $logdate ]: packup_$dirname.tar.gz was created in $backuppath (Took $SECONDS seconds and weighs $(du -sh "$backuppath"/packup_"$dirname".tar.gz | awk '{print $1}'))" >> "$logpath"/$logfile
+    chown root:root "$backuppath"/packup_"$dirname".tar.gz
     chmod 600 "$backuppath"/packup_"$dirname".tar.gz
-else
+elif [ "$failed" = "true" ]; then
     echo "Backup exited with errors and the zipfile was deleted :("
     echo "[ $logdate ]: Backup exited with errors and the tarfile was deleted :(" >> "$logpath"/$logfile
    #echo "BACKUP FAILED!" | mail -s "Backup has failed! Check $logpath/$logfile for the full log!" mail@example.com
     
+    # Remove incomplete backup
     if [ -f "$backuppath"/packup_"$dirname".tar.gz ]; then
         rm -rf "$backuppath"/packup_"$dirname".tar.gz
     fi
     
+    # Send tar errors to the log file
     if [ -s "$logpath"/temp_backups_error.log ]; then
         {
         echo "Encountered the following errors:"
@@ -137,6 +175,7 @@ else
         } >> "$logpath"/$logfile
     fi
     
+    # Remove the temp error log after being appended to the main log file
     if [ -f "$logpath"/temp_backups_error.log ]; then
         rm -f "$logpath"/temp_backups_error.log
     fi
